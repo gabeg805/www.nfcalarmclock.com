9e29100b3738632ed95391e3baa4a2909f715a57
gabeg805 <6011521+gabeg805@users.noreply.github.com>
2023-09-28 22:23:48 -0400

    Tweaked side nav bar, but it is still a work in progress.
    * Tweaked spacing and converted all leading spaces to tabs.

--- header/hamburgerIconOnClick.js
+++ header/hamburgerIconOnClick.js
@@ -1,20 +1,33 @@
+var isSidebarOpen = false;
+
 function myFunction(x)
 {
 	x.classList.toggle("change");
-	openNav();
+
+	if (isSidebarOpen)
+	{
+		closeNav();
+	}
+	else
+	{
+		openNav();
+	}
+
 }
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+/* Set the height of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
+	isSidebarOpen = true;
 	document.getElementById("mySidenav").style.width = "250px";
-	document.getElementById("main").style.marginRight = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
 	document.getElementById("main").style.opacity = "0.3";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/* Set the height of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
+	isSidebarOpen = false;
 	document.getElementById("mySidenav").style.width = "0";
-	document.getElementById("main").style.marginRight = "0";
+	document.getElementById("main").style.marginLeft = "0";
 	document.getElementById("main").style.opacity = "1";
 }
 
